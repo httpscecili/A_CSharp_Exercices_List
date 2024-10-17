5af55925a539f039e155dfa143114e3c36052142
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-17 09:58:32 -0300

    Create StudentsGroup.cs

---
+++ StudentsGroup.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ClassTeams
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int students = 18;
+      int groupSize = 3;
+      
+      Console.WriteLine(students%groupSize);
+    }
+  }
+}
