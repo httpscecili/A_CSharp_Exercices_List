a0b7af2768ed43f6a262190b0457032529647326
CoffeesLia <98719299+httpscecili@users.noreply.github.com>
2024-10-22 13:59:20 -0300

    Create tasks.cs

---
+++ tasks.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ForEachLoop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] todo = {"respond to email", "make wireframe", "program feature", "fix bugs"};
+      foreach (string task in todo)
+      {
+        Console.WriteLine($"[] {task}");
+      }
+    }
+  }
+}
